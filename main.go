e4fdd18308d002b9fd79ef1db7b9697d84185148
叶嘉雨 <jiayu.ye@ele.me>
2017-06-08 23:26:47 +0800

    feat: fe

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.Static("/static", "./static")
-	r.GET("/", func(c *gin.Context) {
-		c.File("static/index.html")
-	})
 	server.SetupAPI(r)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
